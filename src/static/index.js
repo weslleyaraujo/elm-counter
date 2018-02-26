cb0a1029f0b037b0e622c718ef397cd21adc6f90
Weslley Araujo <weslleyaraujo20@gmail.com>
2018-02-26 20:46:05 +0100

    feat(*): remove unused files

--- src/static/index.js
+++ src/static/index.js
@@ -1,4 +1,2 @@
-
-// inject bundled Elm app into div#main
-var Elm = require( '../elm/Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+const Elm = require('../elm/Main');
+Elm.Main.embed(document.getElementById('main'));
